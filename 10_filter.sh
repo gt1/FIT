e40e39b0a752439891e859e77908e85a3ffcf926
German Tischler <tischler@mpi-cbg.de>
2016-01-15 07:09:50 +0100

    update paths of translocations binaries

--- 10_filter.sh
+++ 10_filter.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
+source tools.sh
 
 INSTTHRES=3
 BPTHRESLOW=2
 BPTHRESHIGH=10
-${HOME}/src/translocations/src/falsepositivefilter instthres=${INSTTHRES} bpthreslow=${BPTHRESLOW} bpthres=${BPTHRESHIGH} <stats_25/KMT2A_all.stat.nooverlap.hist.breakpoints_filtered_2.gz \
+${TRANSLOCATIONSDIR}/bin/falsepositivefilter instthres=${INSTTHRES} bpthreslow=${BPTHRESLOW} bpthres=${BPTHRESHIGH} <stats_25/KMT2A_all.stat.nooverlap.hist.breakpoints_filtered_2.gz \
 	> stats_25/KMT2A_all.stat.nooverlap.hist.breakpoints_filtered_2_postfiltered.txt
 
 INSTTHRES=3
 BPTHRESLOW=2
 BPTHRESHIGH=4
-${HOME}/src/translocations/src/falsepositivefilter instthres=${INSTTHRES} bpthreslow=${BPTHRESLOW} bpthres=${BPTHRESHIGH} <stats_25/KMT2A_gene_subset.stat.nooverlap.hist.breakpoints_filtered_2.gz \
+${TRANSLOCATIONSDIR}/bin/falsepositivefilter instthres=${INSTTHRES} bpthreslow=${BPTHRESLOW} bpthres=${BPTHRESHIGH} <stats_25/KMT2A_gene_subset.stat.nooverlap.hist.breakpoints_filtered_2.gz \
 	> stats_25/KMT2A_gene_subset.stat.nooverlap.hist.breakpoints_filtered_2_postfiltered.txt
 
 INSTTHRES=3
 BPTHRESLOW=2
 BPTHRESHIGH=10
-${HOME}/src/translocations/src/falsepositivefilter instthres=${INSTTHRES} bpthreslow=${BPTHRESLOW} bpthres=${BPTHRESHIGH} <stats_25/all.stat.nooverlap.hist.breakpoints_filtered_2.gz \
+${TRANSLOCATIONSDIR}/bin/falsepositivefilter instthres=${INSTTHRES} bpthreslow=${BPTHRESLOW} bpthres=${BPTHRESHIGH} <stats_25/all.stat.nooverlap.hist.breakpoints_filtered_2.gz \
 	> stats_25/all.stat.nooverlap.hist.breakpoints_filtered_2_postfiltered.txt
 
 INSTTHRES=3
 BPTHRESLOW=2
 BPTHRESHIGH=5
-${HOME}/src/translocations/src/falsepositivefilter instthres=${INSTTHRES} bpthreslow=${BPTHRESLOW} bpthres=${BPTHRESHIGH} <stats_25/all.stat.nooverlap.hist.breakpoints_filtered_2.gz
+${TRANSLOCATIONSDIR}/bin/falsepositivefilter instthres=${INSTTHRES} bpthreslow=${BPTHRESLOW} bpthres=${BPTHRESHIGH} <stats_25/all.stat.nooverlap.hist.breakpoints_filtered_2.gz
